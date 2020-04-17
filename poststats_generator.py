545ac5a97ebba4a62435e36ed25842af11724b5d
kostekci <furkankostekci@gmail.com>
2020-04-17 14:11:25 +0300

    Updated VideoPriority

--- poststats_generator.py
+++ poststats_generator.py
@@ -16,13 +16,17 @@ dfq = wordsdf.groupby(['Word'])['VideoName'].size().reset_index(name='VideoCount
 
 #video based sum of VideoCount
 
-xdf = wordsdf.merge(dfq, left_on='Word', right_on='Word').groupby(['VideoName']).sum().sort_values(by='VideoCount', ascending=False).drop(columns=['Count'])
+xdf = wordsdf.merge(dfq, left_on='Word', right_on='Word').groupby(['VideoName']).sum()
+
+xdf['VideoPriority'] = xdf['VideoCount'] / xdf['Count']
+
+xdf = xdf.sort_values(by='VideoPriority', ascending=False)
 
 xdf.to_excel(writer, sheet_name='WhatOrderToFollow')
 
 #What words to check before watching that video
 
-zdf = xdf.merge(wordsdf, left_on='VideoName', right_on='VideoName')
+zdf = xdf.merge(wordsdf, left_on='VideoName', right_on='VideoName').drop(columns=['VideoCount','Count_x']).merge(total_wordsdf, left_on='Word', right_on='Word').sort_values(by=['VideoPriority','Count_y','Count'], ascending=False)
 
 zdf.to_excel(writer, sheet_name='WordsToCheckBeforeWatching')
 
